58cc42f44089bde3c246b6e12fe1be7a216071d1
Estebahn Molina <molina.estebahn@gmail.com>
2022-01-31 16:41:31 -0600

    uncomment so that all code is active

--- js/external.js
+++ js/external.js
@@ -1,28 +1,28 @@
-// "use strict"
-// console.log("Hello from External JavaScript");
-//
-// alert('Welcome to my Website!');
-//
-// let userInput=prompt('whats your favorite color?');
-//
-// alert('No way! ' + userInput + ' is my favorite color too!');
-//
-// let littleMermaid = Number(prompt('How many days did you have The Little Mermaid?')) *3 ;
-//
-//
-// let brotherBear = Number(prompt('How many days did you have Brother Bear?')) *3;
-//
-// let hercules = Number(prompt('How many days did you have Hercules?'))*3;
-//
-// alert('You owe $'+ (hercules+brotherBear+littleMermaid));
-//
-// let googlePay = Number(prompt('How many hours did you work for Google?'))* 400;
-//
-// let amazonPay= Number(prompt('How many hours did you work for Amazon?'))* 380;
-//
-// let faceBookPay=Number(prompt('How many hours did you work for Facebook?'))*350;
-//
-// alert('This check should be $'+(amazonPay+googlePay+faceBookPay));
+ "use strict"
+ console.log("Hello from External JavaScript");
+
+ alert('Welcome to my Website!');
+
+ let userInput=prompt('whats your favorite color?');
+
+ alert('No way! ' + userInput + ' is my favorite color too!');
+
+ let littleMermaid = Number(prompt('How many days did you have The Little Mermaid?')) *3 ;
+
+
+ let brotherBear = Number(prompt('How many days did you have Brother Bear?')) *3;
+
+ let hercules = Number(prompt('How many days did you have Hercules?'))*3;
+
+ alert('You owe $'+ (hercules+brotherBear+littleMermaid));
+
+ let googlePay = Number(prompt('How many hours did you work for Google?'))* 400;
+
+ let amazonPay= Number(prompt('How many hours did you work for Amazon?'))* 380;
+
+ let faceBookPay=Number(prompt('How many hours did you work for Facebook?'))*350;
+
+ alert('This check should be $'+(amazonPay+googlePay+faceBookPay));
 
 let classNotFull=confirm('is there space in the class?');
 

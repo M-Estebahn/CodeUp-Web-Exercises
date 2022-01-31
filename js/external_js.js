d5721a138af44690d3b4ad244febe9af45570ac6
Estebahn Molina <molina.estebahn@gmail.com>
2022-01-31 16:24:57 -0600

    Modify code to include confirms for booleans

--- js/external_js.js
+++ js/external_js.js
@@ -1,41 +1,41 @@
-"use strict"
-console.log("Hello from External JavaScript");
+// "use strict"
+// console.log("Hello from External JavaScript");
+//
+// alert('Welcome to my Website!');
+//
+// let userInput=prompt('whats your favorite color?');
+//
+// alert('No way! ' + userInput + ' is my favorite color too!');
+//
+// let littleMermaid = Number(prompt('How many days did you have The Little Mermaid?')) *3 ;
+//
+//
+// let brotherBear = Number(prompt('How many days did you have Brother Bear?')) *3;
+//
+// let hercules = Number(prompt('How many days did you have Hercules?'))*3;
+//
+// alert('You owe $'+ (hercules+brotherBear+littleMermaid));
+//
+// let googlePay = Number(prompt('How many hours did you work for Google?'))* 400;
+//
+// let amazonPay= Number(prompt('How many hours did you work for Amazon?'))* 380;
+//
+// let faceBookPay=Number(prompt('How many hours did you work for Facebook?'))*350;
+//
+// alert('This check should be $'+(amazonPay+googlePay+faceBookPay));
 
-alert('Welcome to my Website!');
+let classNotFull=confirm('is there space in the class?');
 
-let userInput=prompt('whats your favorite color?');
-
-alert('No way! ' + userInput + ' is my favorite color too!');
-
-let littleMermaid = Number(prompt('How many days did you have The Little Mermaid?')) *3 ;
-
-
-let brotherBear = Number(prompt('How many days did you have Brother Bear?')) *3;
-
-let hercules = Number(prompt('How many days did you have Hercules?'))*3;
-
-alert('You owe $'+ (hercules+brotherBear+littleMermaid));
-
-let googlePay = Number(prompt('How many hours did you work for Google?'))* 400;
-
-let amazonPay= Number(prompt('How many hours did you work for Amazon?'))* 380;
-
-let faceBookPay=Number(prompt('How many hours did you work for Facebook?'))*350;
-
-alert('This check should be $'+(amazonPay+googlePay+faceBookPay));
-
-let classNotFull=true
-
-let noScheduleConflict=true
+let noScheduleConflict=confirm('Is your Schedule clear?');
 
 let canEnrollInClass=(classNotFull && noScheduleConflict)
 console.log(canEnrollInClass)
 
-let premiumMember=true
+let premiumMember=confirm('Is this a premium member?');
 
-let moreThanTwoItems=true
+let moreThanTwoItems= confirm("Are there more than two items?");
 
-let offerNotExpired=true
+let offerNotExpired=confirm('Is the offer still valid');
 
 let applyOffer=(premiumMember || moreThanTwoItems && offerNotExpired);
 

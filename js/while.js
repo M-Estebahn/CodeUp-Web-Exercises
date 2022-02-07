1ddb09121be72e8b6636ad76ee7eed29749ed7cd
Estebahn Molina <molina.estebahn@gmail.com>
2022-02-07 15:49:57 -0600

    complete while.js exercise

--- js/while.js
+++ js/while.js
@@ -10,7 +10,7 @@ var allCones = Math.floor(Math.random() * 50) + 50;
 
 do{
     var conesBought=( Math.floor(Math.random() * 5) + 1);
-    allCones=allCones-conesBought
+
 
     if(allCones === 0) {
         console.log('Yay! I sold them all');
@@ -18,10 +18,9 @@ do{
     }else if (conesBought >= allCones) {
         console.log('I cannot sell you ' + conesBought + ' I only have ' + allCones + ' left',allCones);
         break;
-
     }else (conesBought <= allCones); {
     console.log(conesBought + ' Cones sold',allCones);
-    }
+    }allCones=allCones-conesBought
 
 } while (allCones>=0)
 

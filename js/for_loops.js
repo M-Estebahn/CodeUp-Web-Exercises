b68d817425d78a75471ef968ec80b19ac9abb991
Estebahn Molina <molina.estebahn@gmail.com>
2022-02-08 09:36:08 -0600

    refactor code for repeating number exercise after collaborating with cohorts. leave original code in comments

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,39 +1,43 @@
 'use strict';
 
-//
-//
-// function showMultiplicationTable(input){
-//     var  iteration=0
-//     while (iteration <11){
-//         console.log(input+'x'+iteration+'='+(input*iteration));
-//         iteration++;
-//     }
-// }
-// showMultiplicationTable(7)
-// showMultiplicationTable(8);
-//
-//
-//
-// (function isEvenOrOdd(){
-//     for (var num=0;num<11; num++) {
-//         var random = Math.floor((Math.random() * 200) + 20);
-//         if (random % 2 === 0) {
-//         console.log(random + ' is even')
-//     } else if (random % 2 !== 0)
-//         console.log(random + ' is odd')
-// }})();
-//
+
+
+function showMultiplicationTable(input){
+    var  iteration=0
+    while (iteration <11){
+        console.log(input+'x'+iteration+'='+(input*iteration));
+        iteration++;
+    }
+}
+showMultiplicationTable(7)
+showMultiplicationTable(8);
+
+
+
+(function isEvenOrOdd(){
+    for (var num=0;num<11; num++) {
+        var random = Math.floor((Math.random() * 200) + 20);
+        if (random % 2 === 0) {
+        console.log(random + ' is even')
+    } else if (random % 2 !== 0)
+        console.log(random + ' is odd')
+}})();
+
 // (function runningNumbers(){
 //     for(var iteration=0;iteration<=1000000000;iteration++)
 //         if(iteration===1||iteration===22||iteration===333||iteration===4444||iteration===55555||iteration===666666||iteration===7777777||iteration===88888888||iteration===999999999)
 //             console.log(iteration)
 // })();
-//
-// (function listFives(){
-//     for(var i=100; i>0; i--) {
-//         if (i % 5 === 0)
-//             console.log(i)
-//     }
-//     }
-//  )();
-//
+// code refactor below
+
+for (var repeatingNum=1; repeatingNum<=9; repeatingNum++){
+    console.log(repeatingNum.toString().repeat(repeatingNum))
+}
+(function listFives(){
+    for(var i=100; i>0; i--) {
+        if (i % 5 === 0)
+            console.log(i)
+    }
+    }
+ )();
+

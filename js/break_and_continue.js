3b47e3da724bb290c287bad5cc64f62a80c14919
Estebahn Molina <molina.estebahn@gmail.com>
2022-02-08 12:04:42 -0600

    complete break-continue eexercise,uncomment code in loops.html,create and start js/planets-string.js and split-join.html

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,7 +1,27 @@
 'use strict'
 
-// do {
-//     var userInput = parseInt(prompt('Please enter an Odd number between 1-50.'))
-// } while (userInput<1 && userInput>50 && userInput % 2===0)
-//     alert('Your Input is incorrect')
-//
+
+for(var i=0;i<10;i++) {
+    var userOddNumber = prompt('Please give me an odd number between 1-50')
+    if (userOddNumber % 2 !== 0 && userOddNumber > 1 && userOddNumber < 50) {
+        console.log('Number to skip is ' + userOddNumber)
+        break;
+    }
+}
+
+
+
+var trackedNumber=1
+
+while(trackedNumber<=50){
+        let placeHolder= Number(userOddNumber)
+
+        if (trackedNumber === placeHolder) {
+            console.log('Yikes! skipping number: ' + placeHolder)
+            trackedNumber += 2
+      continue;  }
+
+            console.log('here is an Odd Number '+trackedNumber)
+        trackedNumber +=2
+}
+

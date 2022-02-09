b3dcdeba9c939fab53ed754a72ee4bbda6ac484a
Estebahn Molina <molina.estebahn@gmail.com>
2022-02-09 16:07:14 -0600

    create and complete exercise for js/circle.js and math-js.html

---
+++ js/circle.js
@@ -0,0 +1,45 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            var area=(Math.PI)*(Math.pow(circle.radius,2))
+            // TODO: return the proper value
+                return circle.area=area
+
+        },
+
+
+        logInfo: function (doRounding) {
+            if (doRounding ===true)
+               var info=Math.round(this.getArea())
+            else
+                var info=this.getArea()
+
+            // TODO: complete this method.
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            console.log("Area of a circle with radius: " + this.radius + ", is:"+info);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+        circle.radius=5
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
